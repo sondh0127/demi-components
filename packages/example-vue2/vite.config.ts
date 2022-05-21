7ed2881302d7f306116cdb78defef937985cd6e1
Son Hong Do <sondh0127@gmail.com>
2022-05-21 11:16:59 +0700

    element-plus + element-ui

--- packages/example-vue2/vite.config.ts
+++ packages/example-vue2/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import AutoImport from 'unplugin-auto-import/vite'
 import ScriptSetup from 'unplugin-vue2-script-setup/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +17,12 @@ export default defineConfig({
         '@vue/composition-api',
       ],
       "dts": "./src/auto-imports.d.ts",
-    })
+    }),
+    Components({
+      resolvers: [ElementUiResolver()],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: "./src/components.d.ts",
+    }),
   ],
   resolve: {
     alias: {
--- packages/example-vue3/vite.config.ts
+++ packages/example-vue3/vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +15,11 @@ export default defineConfig({
         'vue'
       ],
       "dts": "./src/auto-imports.d.ts",
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: "./src/components.d.ts",
     }),
   ],
   resolve: {
