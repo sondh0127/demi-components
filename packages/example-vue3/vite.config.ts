6157b9ea3bf3a25b8b74cad7c95f0ca8d975245e
Son Hong Do <sondh0127@gmail.com>
2022-05-23 08:42:04 +0700

    add vue app config

--- packages/example-vue3/vite.config.ts
+++ packages/example-vue3/vite.config.ts
@@ -1,30 +1,45 @@
 import { fileURLToPath, URL } from 'url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import type { ConfigEnv, UserConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { loadEnv } from 'vite'
+import { name } from './package.json'
+import Unocss from 'unocss/vite'
+
+const CWD = process.cwd()
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: [
-        'vue'
-      ],
-      "dts": "./src/auto-imports.d.ts",
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: "./src/components.d.ts",
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const { VITE_BASE_URL } = loadEnv(mode, CWD)
+
+  return {
+    base: VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    plugins: [
+      Vue(),
+      AutoImport({
+        imports: [
+          'vue'
+        ],
+        "dts": "./src/auto-imports.d.ts",
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: "./src/components.d.ts",
+      }),
+      Unocss(),
+    ],
+    server: {
+      port: 4001,
+      open: true,
+    },
   }
-})
+}
